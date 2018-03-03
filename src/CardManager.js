2e74443df003236f5ead5aaeee34d172ee647e7c
Rohit Tiwari <rohittiwarirvt@gmail.com>
2018-03-03 06:47:39 +0530

    Modifying bootstrap to use react-bootstrap

--- src/CardManager.js
+++ src/CardManager.js
@@ -6,15 +6,17 @@ class ContentCard extends Component {
 
   render() {
     return (
-    <div className="card-wrapper">
-      <div className="wrapper">
-        <img alt="test" title="test" src="null"/>
-        <h1>
-          My Card
-        </h1>
-        <p>
-          Explore the history of the classic Lorem Ipsum passage and generate your own text using any number of characters, words, sentences or paragraphs. Commonly used as placeholder text in the graphic and print industries, Lorem Ipsum's origins extend far back to a scrambled Latin passage from Cicero in the middle ages.
-        </p>
+    <div class="col-md-3  col-xs-6">
+      <div className="card-wrapper">
+        <div className="wrapper">
+          <img alt="test" title="test" src="null"/>
+          <h1>
+            My Card
+          </h1>
+          <p>
+            Explore the history of the classic Lorem Ipsum passage and generate your own text using any number of characters, words, sentences or paragraphs. Commonly used as placeholder text in the graphic and print industries, Lorem Ipsum's origins extend far back to a scrambled Latin passage from Cicero in the middle ages.
+          </p>
+        </div>
       </div>
     </div>
     )
